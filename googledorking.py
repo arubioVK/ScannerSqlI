b46714c6dd78b551c9eb1649bb81a274d33dbc13
root <root@localhost.localdomain>
2019-03-08 13:16:19 -0500

    Version usable

--- googledorking.py
+++ googledorking.py
@@ -1,38 +1,24 @@
 #! usr/bin/python
 # -*- coding: utf-8 -*-
 
-import argparse
-import requests
-import urllib
-import json
-import re
+
+import subprocess, time, os, sys,json, urllib, requests, argparse, re
 from bs4 import BeautifulSoup
 
-"""
-
+#Menú
 parser = argparse.ArgumentParser(description='Scanner de Sqlmap + Google Dorking')
 parser.add_argument("-t", "--tor", help="Utilizar anonimáto", action="store_true")
 parser.add_argument("-d","--dork", help="Dork a ejecutar", required=True)
-parser.add_argument("-f", "--file", help="Nombre de archivo de salida")
+parser.add_argument("-f", "--file", help="Nombre de archivo de salida", default="Mozilla/11.0")
+parser.add_argument("-u", "--useragent", help="User-agent",default="Mozilla/11.0")
 args = parser.parse_args()
 
-#UTILIZAR ANONIMATO
-if args.tor:
-	print args.tor
-if args.dork:
-	print "El Dork es "+args.dork
-if args.file:
-	print "Fichero "+args.file
-
-"""
-
-
+#Extracción de las Urls
+lista_vulnerable = list()
 urls = set()
 urlgoogle = 'http://www.google.com/search'
-dork='inurl:gallery.php?id='
-#payload = {'q':args.dork,'start':'0'}
-payload = {'q':dork,'start':'0'}
-user_agent = { 'User-agent' : 'Mozilla/11.0' }
+payload = {'q':args.dork,'start':'0'}
+user_agent = { 'User-agent' : args.useragent }
 r = requests.get(urlgoogle, params = payload, headers = user_agent )
 soup = BeautifulSoup( r.text, 'html.parser' )
 h3tags = soup.find_all( 'h3', class_='r' )
@@ -41,9 +27,44 @@ for h3 in h3tags:
         url = re.search('url\?q=(.+?)\&sa', h3.a['href']).group(1)
 	url = urllib.unquote(url).decode('utf8')
 	urls.add(url)
-	
     except:
         continue
+
+#Comprobación SQLI
+if args.tor:
+    subprocess.call(['service','tor','start'])
+    print "Levantamos TOR"
+for url in urls:
+    esVulnerable=False
+    print "Analizando "+url
+    if args.tor:
+        p=subprocess.Popen(['sqlmap','--tor','-v','0','--random-agent','-u', url,'--batch', '--threads', '10'], stdin=subprocess.PIPE, stdout=subprocess.PIPE , stderr = subprocess.PIPE)
+    else:
+        p=subprocess.Popen(['sqlmap','-v','0','--random-agent','-u', url,'--batch','--threads', '10'], stdin=subprocess.PIPE, stdout=subprocess.PIPE , stderr = subprocess.PIPE)
+    for line in iter(p.stdout.readline, b''):
+        if re.match("back-end DBMS: ",line) is not None:
+            esVulnerable= True
+    if (esVulnerable):
+	print "Vulnerable"
+	print "---"
+	lista_vulnerable.append(url)
+	
+    else:
+	print "NO vulnerable"
+	print "---"
+
+#Escritura en el fichero
+if args.file:
+    f = open('fichero.txt','w')
+    for vulnerable in lista_vulnerable:
+        f.write(vulnerable+"\n")
+    f.close()
+	
+
+
+
+
+
 
 
 
